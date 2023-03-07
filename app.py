fa87095fee8f35d3b671f58f799609a0972ae1b0
loopdusk <127133147+loopdusk@users.noreply.github.com>
2023-03-07 11:25:43 +0700

    commit

--- app.py
+++ app.py
@@ -6,15 +6,25 @@ from io import *
 import glob
 from datetime import datetime
 import os
+from pyrsistent import s
 import wget
 import time
-
+import pathlib
+temp = pathlib.PosixPath
+pathlib.PosixPath = pathlib.WindowsPath
+from fastai.vision.all import (
+    load_learner,
+    PILImage,
+)
 
 def imageInput(device, src):
+    enter=False
     description = ["""โรคใบจุดตากบ (frog_eye) จะพบประจำในพริกและยาสูบ จุดแผลจะกลม กลางแผลมีสีเทา
 ขอบแผลสีน้ำตาล แผลจะกระจายทั่วไป โรคนี้เกิดจากเชื้อเซอโคสะปอร่า (Cercospora) การควบคุมโรคนี้ใช้
 สารชนิดเดียวกับที่ใช้ควบคุมแอนแทรคโนสก็ได้ หรือจะใช้สารประเภทคลอโรธาโรนิล (chlorothalonil) ฉีด
-พ่นสม่ำเสมอขณะระบาด จะได้ผลดี""", """โรคใบเหลือง (yellow_leaf) สาเหตุส่วนใหญ่ที่ทำให้ต้นไม้ใบเหลือง เป็นเพราะรดน้ำมากเกินไป ความชื้นในดินสูง หรือดินแน่น ระบายน้ำยาก วิธีแก้ปัญหาเบื้องต้นคือ ตัดใบส่วนนั้นทิ้งไป จากนั้นเว้นการรดน้ำไปสักระยะ แล้วค่อยกลับมารดน้ำใหม่เมื่อดินแห้ง เช็กง่าย ๆ โดยใช้นิ้วกดลงไปในดินประมาณ 1 นิ้ว หากหน้าดินแห้งก็รดน้ำได้ แต่ถ้าดินยังแฉะก็ควรรอก่อน"""] 
+พ่นสม่ำเสมอขณะระบาด จะได้ผลดี""", """โรคใบเหลือง (yellow_leaf) สาเหตุส่วนใหญ่ที่ทำให้ต้นไม้ใบเหลือง เป็นเพราะรดน้ำมากเกินไป ความชื้นในดินสูง หรือดินแน่น ระบายน้ำยาก วิธีแก้ปัญหาเบื้องต้นคือ ตัดใบส่วนนั้นทิ้งไป จากนั้นเว้นการรดน้ำไปสักระยะ แล้วค่อยกลับมารดน้ำใหม่เมื่อดินแห้ง เช็กง่าย ๆ โดยใช้นิ้วกดลงไปในดินประมาณ 1 นิ้ว หากหน้าดินแห้งก็รดน้ำได้ แต่ถ้าดินยังแฉะก็ควรรอก่อน"""]
+    GOOD_OR_BAD = pathlib.Path("models/good_or_bad.pkl")
+    good_or_bad = load_learner(GOOD_OR_BAD)
     if src == 'จากเครื่องของคุณ':
         image_file = st.file_uploader("อัปโหลดภาพ", type=['png', 'jpeg', 'jpg'])
         col1, col2 = st.columns(2)
@@ -28,25 +38,27 @@ def imageInput(device, src):
             with open(imgpath, mode="wb") as f:
                 f.write(image_file.getbuffer())
             # call Model prediction--
-           
-            model = torch.hub.load('ultralytics/yolov5', 'custom', path='models/best.pt', force_reload=True)
-            # model.cuda() if device == 'cuda' else model.cpu()
-            model.cpu()
-            pred = model(imgpath)
-            pred.render()  # render bbox in image
-            for im in pred.ims:
-                im_base64 = Image.fromarray(im)
-                im_base64.save(outputpath)
-            # --Display predicton
-            img_ = Image.open(outputpath)
-            with col2:
-                st.image(img_, caption='Model Prediction', use_column_width='always')
-        st.markdown(f"<div style='background:#FF6F61;padding:0 20px 0 20px;border-radius:10px 10px 0 0;'><h1 style='color:#000000'>โรคใบจุดตากบ</h1></div>", unsafe_allow_html=True)
-        st.markdown(f"<div style='background:none;padding:30px;border-radius:0 0 10px 10px;border:2px solid #FF6F61'>{description[0]}</div>", unsafe_allow_html=True)
-        st.markdown("<div><br></div>", unsafe_allow_html=True)
-        st.markdown(f"<div style='background:#FF6F61;padding:0 20px 0 20px;border-radius:10px 10px 0 0;'><h1 style='color:#000000'>โรคใบเหลือง</h1></div>", unsafe_allow_html=True)
-        st.markdown(f"<div style='background:none;padding:30px;border-radius:0 0 10px 10px;border:2px solid #FF6F61'>{description[1]}</div>", unsafe_allow_html=True)
-
+            checkleaf = good_or_bad.predict(imgpath)
+            if checkleaf[0] == 'good' :
+                model = torch.hub.load('ultralytics/yolov5', 'custom', path='models/best.pt', force_reload=True)
+                # model.cuda() if device == 'cuda' else model.cpu()
+                model.cpu()
+                pred = model(imgpath)
+                pred.render()  # render bbox in image
+                for im in pred.ims:
+                    im_base64 = Image.fromarray(im)
+                    im_base64.save(outputpath)
+                # --Display predicton
+                img_ = Image.open(outputpath)
+                with col2:
+                    st.image(img_, caption='Model Prediction', use_column_width='always')
+                st.markdown(f"<div style='background:#FF6F61;padding:0 20px 0 20px;border-radius:10px 10px 0 0;'><h1 style='color:#000000'>โรคใบจุดตากบ</h1></div>", unsafe_allow_html=True)
+                st.markdown(f"<div style='background:none;padding:30px;border-radius:0 0 10px 10px;border:2px solid #FF6F61'>{description[0]}</div>", unsafe_allow_html=True)
+                st.markdown("<div><br></div>", unsafe_allow_html=True)
+                st.markdown(f"<div style='background:#FF6F61;padding:0 20px 0 20px;border-radius:10px 10px 0 0;'><h1 style='color:#000000'>โรคใบเหลือง</h1></div>", unsafe_allow_html=True)
+                st.markdown(f"<div style='background:none;padding:30px;border-radius:0 0 10px 10px;border:2px solid #FF6F61'>{description[1]}</div>", unsafe_allow_html=True)
+            else:
+                st.error('ไม่สามารถใช้ภาพนี้ได้ กรุณาอัปโหลดใหม่อีกครั้ง')
     elif src == 'จากไฟล์ของเว็บไซต์':
         # Image selector slider
         test_images = os.listdir('data/images/')
@@ -70,11 +82,13 @@ def imageInput(device, src):
                     # --Display predicton
                     img_ = Image.open(os.path.join('data/outputs', os.path.basename(image_file)))
                     st.image(img_, caption='Model Prediction', use_column_width='always')
-        st.markdown(f"<div style='background:#FF6F61;padding:0 20px 0 20px;border-radius:10px 10px 0 0;'><h1 style='color:#000000'>โรคใบจุดตากบ</h1></div>", unsafe_allow_html=True)
-        st.markdown(f"<div style='background:none;padding:30px;border-radius:0 0 10px 10px;border:2px solid #FF6F61'>{description[0]}</div>", unsafe_allow_html=True)
-        st.markdown("<div><br></div>", unsafe_allow_html=True)
-        st.markdown(f"<div style='background:#FF6F61;padding:0 20px 0 20px;border-radius:10px 10px 0 0;'><h1 style='color:#000000'>โรคใบเหลือง</h1></div>", unsafe_allow_html=True)
-        st.markdown(f"<div style='background:none;padding:30px;border-radius:0 0 10px 10px;border:2px solid #FF6F61'>{description[1]}</div>", unsafe_allow_html=True)
+                    enter=True
+        if enter==True:
+            st.markdown(f"<div style='background:#FF6F61;padding:0 20px 0 20px;border-radius:10px 10px 0 0;align:center'><h1 style='color:#000000'>โรคใบจุดตากบ</h1></div>", unsafe_allow_html=True)
+            st.markdown(f"<div style='background:none;padding:30px;border-radius:0 0 10px 10px;border:2px solid #FF6F61'>{description[0]}</div>", unsafe_allow_html=True)
+            st.markdown("<div><br></div>", unsafe_allow_html=True)
+            st.markdown(f"<div style='background:#FF6F61;padding:0 20px 0 20px;border-radius:10px 10px 0 0;'><h1 style='color:#000000'>โรคใบเหลือง</h1></div>", unsafe_allow_html=True)
+            st.markdown(f"<div style='background:none;padding:30px;border-radius:0 0 10px 10px;border:2px solid #FF6F61'>{description[1]}</div>", unsafe_allow_html=True)
                     # if disease_name == "yellow_leaf":
                     #     st.markdown(description[0], unsafe_allow_html=True)
                     # elif disease_name == "frog_eye":
